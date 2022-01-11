a6fd6e39475c26e8e32255b5e97b8de676e699e4
dev-yoga <spin_shoppe@icloud.com>
2022-01-11 13:41:11 -0800

    heyheyhey

--- hello.js
+++ hello.js
@@ -1,2 +1,18 @@
-console.log('Hello World!');
+//this function will take in two arguments, string and num
+const repeatString = function(word, num) {
+    // returns error with negative numbers
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+    // create a variable to hold the string you're going to return
+        let onRepeat = '';
+    // create a loop that repeats the given number of times
+        for (let i = 0; i < num; i++) {
+    // add the given string to the result on each loop
+            onRepeat += word;
+        }
+        return onRepeat;
+    }
+};
+    
 
