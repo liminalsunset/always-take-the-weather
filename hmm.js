1d654b5cbe94ad9bc520c4207829413085ba7915
dev-yoga <spin_shoppe@icloud.com>
2022-01-26 17:05:49 -0800

    It worked :D

--- hmm.js
+++ hmm.js
@@ -1,6 +1,11 @@
 const sumAll = function(num1, num2) {
+    // let sum = 0;
+    // for (let i = num1; i < num2; i++) {
+    //     sum += i;
+    // }
+    // return sum;
     let sum = 0;
-    for (let i = num1; i < num2; i++) {
+    for (let i = num1; i < num2 + 1; i++) {
         sum += i;
     }
     return sum;
