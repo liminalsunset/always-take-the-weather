7aa5919ef9aec5eb372d5caf7267efada343911a
dev-yoga <spin_shoppe@icloud.com>
2022-01-28 20:15:38 -0800

    Flip

--- hmm.js
+++ hmm.js
@@ -4,6 +4,11 @@ const sumAll = function(num1, num2) {
     //     sum += i;
     // }
     // return sum;
+    if (num1 > num2) {
+        const flip = num1;
+        num1 = num2;
+        num2 = flip;
+    }
     let sum = 0;
     for (let i = num1; i < num2 + 1; i++) {
         sum += i;
@@ -11,4 +16,4 @@ const sumAll = function(num1, num2) {
     return sum;
 };
 
-console.log(sumAll(1, 4));
+console.log(sumAll(123, 1));
