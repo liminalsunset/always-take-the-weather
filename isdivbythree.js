7bd32569c74fe0ecaf315a13c6748ca16c420b7a
dev-yoga <spin_shoppe@icloud.com>
2022-01-31 10:17:40 -0800

    Stitches

--- isdivbythree.js
+++ isdivbythree.js
@@ -1,7 +1,7 @@
 let divisibleByThree = function(num) {
     let stitches = (num % 3);
-    return `Your remainder is ${stitches}`;
+    return `Your remainder is ${stitches} which means you need ${3 - stitches} more stitch(es).`;
 };
 // 264
 
-console.log(divisibleByThree(264));
+console.log(divisibleByThree(263));
