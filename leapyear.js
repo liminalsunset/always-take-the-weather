735b56582fd7c13abe2aa7d496cd24629b329335
dev-yoga <spin_shoppe@icloud.com>
2022-02-01 13:04:46 -0800

    Unless

--- leapyear.js
+++ leapyear.js
@@ -1,5 +1,7 @@
 const leapYears = function(num) {
-    if ((num > 0) && (num / 4) && (num % 100 !== 0)) {
+    if ((num > 0) && (num / 4) && (num % 100 !== 0)
+        // unless the number is divisible by 400 :D
+    ) {
         return 'It might be a leap year.';
     } else {
         return 'Probably not a leap year.';
