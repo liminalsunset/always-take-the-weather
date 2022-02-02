fbb340a5ef1ee7d4c3526ba8bcb95ae050a7deb5
dev-yoga <spin_shoppe@icloud.com>
2022-02-01 20:59:28 -0800

    Works

--- leapyear.js
+++ leapyear.js
@@ -1,14 +1,15 @@
 const leapYears = function(num) {
-    if ((num > 0) && (num / 4) && (num % 100 === 0) && (num % 400 === 0)){
+    // leap years are divisible by 4
+    // years divisible by 100 are not leap years
+    // unless they are also divisible by 400
+    if (num % 4 === 0 && (num % 100 !== 0 ||num % 400 === 0)) {
         return 'It might be a leap year.';
-    } else if ((num > 0) && (num / 4) && (num % 100 !== 0) && (num % 400 !== 0)){
-        return 'Probably not a leap year.';
     } else {
-        return 'Please play again.';
+        return 'Not a leap year.';
     }
 };
 
 
-console.log(leapYears(1984));
-console.log(leapYears(1900));
-console.log(leapYears(2000));
+console.log(leapYears(1984)); // leap year
+console.log(leapYears(1900)); // not a leap year
+console.log(leapYears(2000)); // leap year
