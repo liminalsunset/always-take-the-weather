f2da730719c249e4f76157944ea41bded912e331
dev-yoga <spin_shoppe@icloud.com>
2022-02-01 21:58:08 -0800

    temp :)

--- temperature.js
+++ temperature.js
@@ -1,9 +1,9 @@
 const ftoc = function(fahrenheit) {
-    return (fahrenheit - 32) * 5 / 9;
+    return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
 const ctof = function(celsius) {
-    return (celsius * 9 / 5) + 32;
+    return Math.round(((celsius * 9 / 5) + 32) * 10) / 10;
 };
 
 console.log(ftoc(32));
