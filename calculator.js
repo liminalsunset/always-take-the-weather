04f951ca33607347fa2c56c8420a6c87da8e4976
dev-yoga <spin_shoppe@icloud.com>
2022-02-03 06:16:46 -0800

    changes to multiply using reduce

--- calculator.js
+++ calculator.js
@@ -14,8 +14,14 @@ const sum = function(arr) {
     return sumOfArray;
 };
   
-const multiply = function(num1, num2) {
-    return num1 * num2;
+const multiply = function(arr) {
+    // let multSum = 0;
+    // for (let i = 0; i < arr.length; i++){
+    //     multSum += multSum * arr[i];
+    // } return multSum;
+    return arr.length
+        ? arr.reduce((accumulator, nextItem) => accumulator * nextItem)
+        : 0;
 };
   
 const power = function(num1, num2) {
@@ -34,6 +40,6 @@ const factorial = function(num) {
 console.log(add(1, 2));
 console.log(subtract(2, 1));
 console.log(sum(1, 1));
-console.log(multiply(3, 3));
+console.log(multiply([3, 3]));
 console.log(power(1, 2));
 console.log(factorial(5));
