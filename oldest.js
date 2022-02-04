d70750c8473602d4a81bf7229ca48c31b515b08b
dev-yoga <spin_shoppe@icloud.com>
2022-02-04 15:21:48 -0800

    Fix me

---
+++ oldest.js
@@ -0,0 +1,35 @@
+const people = [
+    {
+        name: 'Hyacinth',
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+    },
+    {
+        name: 'Lupine',
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: 'Chrysanthemum',
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+];
+
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
+};
+
+const getAge = function(birth, death){
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
+
+console.log(findTheOldest((people).name));
+console.log((people).name);
