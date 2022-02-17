dbb236290221dc56c300549984f0a74bdb521d1a
dev-yoga <spin_shoppe@icloud.com>
2022-02-17 14:25:33 -0800

    a red paragraph

--- app.js
+++ app.js
@@ -5,4 +5,11 @@ const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
+const pcontent = document.createElement('p');
+content.classList.add('pcontent');
+pcontent.textContent = 'This is paragraph content.';
+pcontent.style.color = 'red';
 container.appendChild(content);
+container.appendChild(pcontent);
+
+
