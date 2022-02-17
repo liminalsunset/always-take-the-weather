f3ed8b244043e6bc9ed7059588d707090647b480
dev-yoga <spin_shoppe@icloud.com>
2022-02-17 14:36:42 -0800

    a blue h3 and a new div

--- app.js
+++ app.js
@@ -9,7 +9,21 @@ const pcontent = document.createElement('p');
 content.classList.add('pcontent');
 pcontent.textContent = 'This is paragraph content.';
 pcontent.style.color = 'red';
+
+const h3content = document.createElement('h3');
+content.classList.add('h3content');
+h3content.textContent = 'I\'m a blue h3!';
+h3content.style.color = 'blue';
+
+const newdiv = document.createElement('div');
+content.classList.add('newdiv');
+newdiv.style.backgroundColor = 'pink';
+newdiv.style.border = ' solid black';
+newdiv.textContent = 'Test';
+
+
 container.appendChild(content);
 container.appendChild(pcontent);
-
+container.appendChild(h3content);
+container.appendChild(newdiv);
 
