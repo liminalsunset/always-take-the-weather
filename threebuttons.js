3b21e1fd0ad98730f133ab2697686359da0bf4dc
dev-yoga <spin_shoppe@icloud.com>
2022-02-20 14:45:55 -0800

    Two buttons

--- threebuttons.js
+++ threebuttons.js
@@ -1,8 +1,11 @@
 const container = document.querySelector('#container');
 
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert('Hello World');
 const content = document.createElement('div');
 
 content.classList.add('content');
 content.textContent = 'This is contained!';
 
 container.appendChild(content);
+
