3536e98a9a0fa731527eba34da7dbf2f0ddbdab1
dev-yoga <spin_shoppe@icloud.com>
2022-02-20 14:48:22 -0800

    Three buttons

--- threebuttons.js
+++ threebuttons.js
@@ -2,6 +2,12 @@ const container = document.querySelector('#container');
 
 const btn = document.querySelector('#btn');
 btn.onclick = () => alert('Hello World');
+
+const btn2 = document.querySelector('#btn2');
+btn2.addEventListener('click', () => {
+    alert('Hello World!');
+});
+
 const content = document.createElement('div');
 
 content.classList.add('content');
